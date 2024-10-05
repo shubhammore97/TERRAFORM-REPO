3314abfcba65135c3a5a3d2e3cedb908eb0e78ae
shubhammore97 <48156211+shubhammore97@users.noreply.github.com>
2024-10-05 19:09:55 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,13 +2,13 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws-instance" "one" {
+resource "aws_instance" "one" {
   ami                    = "ami-00f251754ac5da7f0"
   instance_type          = "t2.micro"
   key_name               = "shubham_perm"
   vpc_security_group_ids = ["sg-0b7947b90b29aed50"]
 
-  tags {
+  tags = {
 
     Name = "shubham_server"
   }
