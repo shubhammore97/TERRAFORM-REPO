d3a08da19049d5dae3f3040baecfc6d199dec66a
shubhammore97 <48156211+shubhammore97@users.noreply.github.com>
2024-10-05 18:44:56 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,15 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws-instance" "one" {
+  ami                    = "ami-00f251754ac5da7f0"
+  instance_type          = "t2.micro"
+  key_name               = "shubham_perm"
+  vpc_security_group_ids = ["sg-0b7947b90b29aed50"]
+
+  tags {
+
+    Name = "shubham_server"
+  }
+}
